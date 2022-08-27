142496f1bc5fa937c88285b674311600abdc19d5
kit <kit@example.com>
2022-08-27 14:52:01 +0000

    build(kit): install features

---
+++ .kit/features/0-display-welcome-message/install-into-client.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]:-$0}"; )" &> /dev/null && pwd 2> /dev/null; )";
+
+echo "*********************************************************************"
+printf "\xF0\x9F\x98\xBA Meow, Kitty here!\n"
+echo
+echo "I am here to help you be successful in the GitKit activities."
+echo "You will hear from me every once in a while when I think you"
+echo "might need some help."
+echo "*********************************************************************"
